72ff8d032750310caf37d6dc2925d0d6fbc47bd7
Veronika Savluk <s_veronica@ukr.net>
2022-10-09 17:27:45 +0300

    Create Statistic.styled.js

---
+++ src/components/statistic/Statistic.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Options = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  margin: 0;
+  padding: 0;
+`;
+
+export const Option = styled.li`
+padding-bottom: 10px;
+list-style: none;
+`;
