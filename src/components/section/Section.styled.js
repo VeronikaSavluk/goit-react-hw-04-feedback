56d1087cf0aaf432503affa5013d9af811a1bfba
Veronika Savluk <s_veronica@ukr.net>
2022-10-09 17:27:36 +0300

    Create Section.styled.js

---
+++ src/components/section/Section.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Card = styled.section`
+padding: 0 30px;
+`;
+
+export const Title = styled.h2`
+font-weight: 700;
+margin: 0;
+padding: 30px 0;
+`;
