45b162ce9afd36f1a1c97fdfda8eb8d59f6d7a00
Veronika Savluk <s_veronica@ukr.net>
2022-10-09 18:51:59 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -17,15 +17,15 @@ const App = () => {
   const onLeaveFeedback = (e) => {
     switch (e.target.textContent) {
       case "good":
-        setGood(good + 1);
+        setGood(good => good + 1);
         break;
 
       case "neutral":
-        setNeutral(neutral + 1);
+        setNeutral(neutral => neutral + 1);
         break;
 
       case "bad":
-        setBad(bad + 1);
+        setBad(bad => bad + 1);
         break;
       default:
         return;
