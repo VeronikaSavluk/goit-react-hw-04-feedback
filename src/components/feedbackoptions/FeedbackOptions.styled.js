ffc728665557caa64efc1a529066fb64bf489f63
Veronika Savluk <s_veronica@ukr.net>
2022-10-09 17:27:20 +0300

    Create FeedbackOptions.styled.js

---
+++ src/components/feedbackoptions/FeedbackOptions.styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Buttons = styled.div`
+    display: flex;
+    margin: 0;
+    padding: 0;
+    aling-item: center;
+    `;
+
+export const Btn = styled.button`
+    display: block;
+    width: 65px;
+    font-size: 10px;
+    margin-left: 10px;
+    padding: 4px;
+    text-transform: capitalize;
+    border: 1px solid #6495ED;
+    border-radius: 5px;
+    background: #6495ED;
+    color: white;
+`;
